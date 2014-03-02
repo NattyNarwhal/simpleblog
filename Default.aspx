0a80d491a974af3a0232e1918fd0c48be7201ad2
Calvin B <calvin@imaginarycode.com>
2014-03-02 13:01:48 -0400

    Switch to repeater
    It could be better done, but it was better than the previous inline thing

--- Default.aspx
+++ Default.aspx
@@ -2,5 +2,9 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="Server">
-	<ul><% GetPosts(); %></ul>
+  <asp:Repeater id="posts" runat="server">
+    <HeaderTemplate><ul></HeaderTemplate>
+      <ItemTemplate><li><a href="View.aspx?p=<%# DataBinder.Eval(Container.DataItem, "Title") %>"><%# DataBinder.Eval(Container.DataItem, "Title") %></a> - <%# DataBinder.Eval(Container.DataItem, "Posted") %></li></ItemTemplate>
+      <FooterTemplate></ul></FooterTemplate>
+  </asp:Repeater>
 </asp:Content>
--- Default.aspx.cs
+++ Default.aspx.cs
@@ -4,23 +4,34 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.Configuration;
+using System.Collections.Generic;
 
 namespace SimpleBlog {
 	public partial class Blog : Page {
 		protected void Page_Load(object sender, EventArgs e) {
-			// Set some vars in the future
+			GetPosts();
 		}
 		
 		// Get posts in posts dir
 		public void GetPosts() {
 			// TODO: grouped version, alternate modes, shit like that
+			// TODO: is a repeater the best?
+			var postList = new List<Post>();
 			var di = new DirectoryInfo(WebConfigurationManager.AppSettings["PostsDirectory"]);
 			var files = di.GetFiles().OrderByDescending(f =>
 				f.LastWriteTime).ToList();
 			foreach (var fi in files) {
-				var n = fi.Name;
-				Response.Write("<li><a href='View.aspx?p=" + n + "'>" + n + "</a> - " + fi.LastWriteTime.ToString() + "</li>");
+				postList.Add(new Post{ Title = fi.Name, Posted = fi.LastWriteTime });
 			}
+			posts.DataSource = postList;
+			posts.DataBind();
 		}
+	}
+
+	public class Post
+	{
+		public string Title { get; set; }
+		public DateTime Posted { get; set; }
+		public Post() { }
 	}
 }
